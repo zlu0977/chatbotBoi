5a315909ec00deea32a72dfadb31822422c7cfe1
Student6 <Student6@10.8.33.148>
2016-09-28 14:02:27 -0400

    WendyMath

---
+++ ChatBotBoi/src/groupFiles/WendyMath.java
@@ -0,0 +1,17 @@
+package groupFiles;
+
+public class WendyMath implements Chatbot {
+
+	@Override
+	public void talk() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
