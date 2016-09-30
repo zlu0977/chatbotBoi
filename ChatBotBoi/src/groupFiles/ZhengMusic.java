8d4701b73ed31a51b17adf8f81a72bd8f5d15873
Student 8 <Student 8@10.8.33.168>
2016-09-30 12:39:36 -0400

    ZhengMusic

---
+++ ChatBotBoi/src/groupFiles/ZhengMusic.java
@@ -0,0 +1,17 @@
+package groupFiles;
+
+public class ZhengMusic implements Chatbot {
+
+	@Override
+	public void talk() {
+		
+
+	}
+
+	@Override
+	public boolean isTriggered(String userInput) {
+		
+		return false;
+	}
+
+}
