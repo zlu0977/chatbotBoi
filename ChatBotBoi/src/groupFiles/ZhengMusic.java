af6f50d11eb098446d165e6229c8c2d4857fc0a8
zlu0977 <zlu0977@bths.edu>
2016-09-30 12:51:52 -0400

    Update ZhengMusic.java

--- ChatBotBoi/src/groupFiles/ZhengMusic.java
+++ ChatBotBoi/src/groupFiles/ZhengMusic.java
@@ -8,6 +8,11 @@ public class ZhengMusic implements Chatbot {
 	}
   
 	public boolean isTriggered(String userInput) {
+		String[] triggers = {"music", "lyric", "lyrics", "song", "songs"};
+		
+		for(int i = 0; i < triggers.length; i ++)
+			if(ZhengMain.findKeyword(userInput, triggers[i], 0) >= 0)
+				return true;
 		
 		return false;
 	}
