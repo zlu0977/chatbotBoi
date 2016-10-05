88b2db9ff6140108c7c2538a9909d51bcaa3e378
Student8 <Student8@10.8.33.156>
2016-10-05 14:04:08 -0400

    boooooooooooooooooooooooook.

--- ChatBotBoi/src/groupFiles/RouseBook.java
+++ ChatBotBoi/src/groupFiles/RouseBook.java
@@ -3,21 +3,21 @@ package groupFiles;
 public class RouseBook implements Chatbot{
 
 	private String[] keywords = {"book","books","novel","novels","manga"};
-	private String response;
+	private String bookResponse;
 	private boolean inBookLoop;
+	private String[] books = {"Flight", "The Dresden Files"};
 	
 	public void talk() {
 		inBookLoop = true;
-		print("Do you read?");
-		response = ZhengMain.promptInput();
-		if(ZhengMain.findKeyword(response, "yes", 0) >= 0){
+		ZhengMain.syso("Do you read?");
+		bookResponse = ZhengMain.promptInput();
+		if(ZhengMain.findKeyword(bookResponse, "yes", 0) >= 0){
 			while(inBookLoop){
-				print("Name something you like to read.");
-				response = ZhengMain.promptInput();
+				bookResponse = ZhengMain.promptInput();
 			}
 		}else{
 			inBookLoop = false;
-			print("Oh... and here I thought we had something I common.");
+			ZhengMain.syso("Oh... and here I thought we had something I common.");
 			ZhengMain.promptForever();
 		}
 	}
@@ -31,8 +31,43 @@ public class RouseBook implements Chatbot{
 		return false;
 	}
 	
-	public void print(String s){
-		System.out.println(s);
+	public void printResponse(String userIn){
+		int repeat = 1;
+		for(int i = 0; i < books.length; i++){
+			
+			if(ZhengMain.findKeyword(userIn, books[i], 0) >= 0)
+			{
+				talkBook();
+				return;
+			}
+			else 
+			{	
+				repeat++;
+				talkBad(repeat);
+			}	
+			
+		}
+	}
+
+	private void talkBad(int irritationLevel) {
+		String[] negatives = {"I don't want to talk about that.", "I would prefer to talk about something else.", "Let's change the conversation.", "LET'S. TALK. ABOUT. BOOKS.", "I'm on strike now."};
+		if(irritationLevel > negatives.length){
+			ZhengMain.syso(negatives[irritationLevel-1]);
+		}else{
+			ZhengMain.syso(negatives[negatives.length-1]);
+		}
+	}
+
+	private void talkBook() {
+		ZhengMain.syso("Do you want to talk about Flight or the Dresden Files?");
+		bookResponse = ZhengMain.promptInput();
+		if(ZhengMain.findKeyword(bookResponse, "Flight", 0) >= 0){
+			flightTalk();
+		}else if(ZhengMain.findKeyword(bookResponse, "the Dresden Files", 0) >= 0){
+			dresTalk();
+		}else{
+			"";
+		}
 	}
 
 }
